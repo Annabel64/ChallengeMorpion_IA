39b177dcaaa1c987f2c6c07d1e2c0195a787d6ad
Annabel64 <julia64.adams@gmail.com>
2021-05-05 13:41:44 +0200

    Terminal_Test et minmax

--- BatailleIA_Rendu.py
+++ BatailleIA_Rendu.py
@@ -4,6 +4,7 @@ Created on Sat May  1 13:39:45 2021
 
 @author: rayan
 """
+import numpy as np
 
 #%% A faire dans l'ordre
 
@@ -14,10 +15,7 @@ Created on Sat May  1 13:39:45 2021
 #euristique (repérer là où est la densité la plus importante de pions ennemis, pour repérer la zone la plus "dangereuse" pour nous))
 #l'intégrer
 
-#%%
-
-
-import numpy as np
+#%% classe Plateau
 
 # symbolJoueur vaut soit "x" soit "o"
 
@@ -39,70 +37,70 @@ class Plateau:
             msg+="\n"
         return msg
 
-
-# On renvoie une liste contenant toutes les positions qui ne sont pas occupés
-# p est le plateau
-def Action(p):
-	l = []
-	for i in range(p.shape[0]):
-		for j in range(p.shape[1]):	
-			if p[i][j] == None:
-				l.append([i,j])
-	return l
-
-def Result(p,a,symbolJoueur):
-    """actualise le plateau du jeu"""
-	p[a[0]][a[1]]=symbolJoueur
-	return p
+#%% Méthodes pour jouer
 
 
-# i,j sont les coord du pion qui vient juste d'être posé
-def Terminal_Test(p):
+def Action(plateau):
+    """On renvoie une liste contenant toutes les positions qui ne sont pas occupées"""
+    l = []
+    for i in range(plateau.tab.shape[0]):
+        for j in range(plateau.tab.shape[1]):	
+            if plateau.tab[i][j] == None:
+                l.append([i,j])
+    return l
+
+def Joue(plateau,a,symbolJoueur):
+    """actualise le plateau du jeu en ajoutant le pion du joueur en a=[x,y]"""
+    x,y=a[0],a[1]
+    plateau.tab[x][y]=symbolJoueur
+    return plateau
+
+
+def Terminal_Test(plateau):
     """renvoie True si il y a un gagnant, False si il n'y en a pas et None si match nul"""
+    # i,j sont les coord du pion qui vient juste d'être posé
     # on vérifie que le plateau n'est pas entièrement rempli
     plateauRempli=False
-    caseVide=[x[i] for x in p for i in range(12) if x[i]==None]
+    caseVide=[x[i] for x in plateau.tab for i in range(12) if x[i]==None]
     if len(caseVide)==0:
         plateauRempli=True  
 
     # on vérifie sur les lignes qu'il n'y ait pas de gagnant
     presenceGagnant=False
-    for i in range(p.shape[0]):
-        for j in range(p.shape[1]-3):
-            if(p[i][j]==p[i][j+1]==p[i][j+2]==p[i][j+3]!=None):
+    for i in range(plateau.tab.shape[0]):
+        for j in range(plateau.tab.shape[1]-3):
+            if(plateau.tab[i][j]==plateau.tab[i][j+1]==plateau.tab[i][j+2]==plateau.tab[i][j+3]!=None):
                 presenceGagnant=True
 
     # on vérifie sur les colonnes qu'il n'y ait pas de gagnant   
-    for j in range(p.shape[1]):
-        for i in range(p.shape[0]-3):
-            if(p[i][j]==p[i+1][j]==p[i+2][j]==p[i+3][j]!=None):
+    for j in range(plateau.tab.shape[1]):
+        for i in range(plateau.tab.shape[0]-3):
+            if(plateau.tab[i][j]==plateau.tab[i+1][j]==plateau.tab[i+2][j]==plateau.tab[i+3][j]!=None):
                 presenceGagnant=True
 
     # on vérifie sur les diagonales à pentes positives qu'il n'y ait pas de gagnant
-    for x in range(3,p.shape[0]):
-        for y in range(3,p.shape[1]):
+    for x in range(3,plateau.tab.shape[0]):
+        for y in range(3,plateau.tab.shape[1]):
             for k in range (4):
                 if (x-3+k>-1 and x-2+k>-1 and x-1+k>-1 and x+k>-1 and x-3+k<12 and x-2+k<12 and x-1+k<12 and x+k<12 and y-3+k>-1 and y-2+k>-1 and y-1+k>-1 and y+k>-1 and y-3+k<12 and y-2+k<12 and y-1+k<12 and y+k<12):
-                    if (p[x-3+k][y-3+k]==p[x-2+k][y-2+k]==p[x-1+k][y-1+k]==p[x+k][y+k]!=None):
+                    if (plateau.tab[x-3+k][y-3+k]==plateau.tab[x-2+k][y-2+k]==plateau.tab[x-1+k][y-1+k]==plateau.tab[x+k][y+k]!=None):
                         presenceGagnant=True
     
     # on vérifie sur les diagonales à pentes négatives qu'il n'y ait pas de gagnant
-    for x in range(3,p.shape[0]):
-        for y in range(p.shape[1]-3):
+    for x in range(3,plateau.tab.shape[0]):
+        for y in range(plateau.tab.shape[1]-3):
             for k in range (4):
                 if (x-3+k>-1 and x-2+k>-1 and x-1+k>-1 and x+k>-1 and x-3+k<12 and x-2+k<12 and x-1+k<12 and x+k<12 and y+3-k>-1 and y+2-k>-1 and y+1-k>-1 and y-k>-1 and y+3-k<12 and y+2-k<12 and y+1-k<12 and y-k<12):
-                    if (p[x-3+k][y+3-k]==p[x-2+k][y+2-k]==p[x-1+k][y+1-k]==p[x+k][y-k]!=None):                            
+                    if (plateau.tab[x-3+k][y+3-k]==plateau.tab[x-2+k][y+2-k]==plateau.tab[x-1+k][y+1-k]==plateau.tab[x+k][y-k]!=None):                            
                         presenceGagnant=True
 
-    return None if plateauRempli==True and presenceGagnant==False else presenceGagnant # on affiche match None si il y a un match nul
+    # on affiche match None si il y a un match nul
+    return None if plateauRempli==True and presenceGagnant==False else presenceGagnant
 
-# Un joueur joue, puis on appelle TerminalTest. Si TerminalTest retourne None alors c'est un match nul,
-# Si elle renvoie False, il n'y a pas encore de gagnant et le plateau n'est pas rempli donc la partie peut continuer
-# Si elle renvoie True alors le joueur qui vient de jouer à gagné.
 
-def Utility(p,symbolJoueur):#n'est utlisé que sur un plateau dont la partie est fini
+def Utility(plateau,symbolJoueur):#n'est utlisé que sur un plateau dont la partie est fini
     """met 0 pour un match nul, 1 si l'IA gagne et -1 si elle perd"""
-    resultat=Terminal_Test(p)
+    resultat=Terminal_Test(plateau)
     score=0
     if (resultat==True and symbolJoueur=="x"):
         score=1
@@ -110,118 +108,98 @@ def Utility(p,symbolJoueur):#n'est utlisé que sur un plateau dont la partie est
         score=-1
     return score
 
+# def MaxValue(plateau,symbolJoueur):
+#     """retourne le max des options que l'adversaire nous laisse jouer (parmis tous les min restant)"""
+#     value=-200
+#     if (Terminal_Test(plateau)!=False):
+#         print("fin")
+#         return Utility(plateau,symbolJoueur)
+#     else:
+#         for a in Action(plateau):
+#             value=max(value,MinValue(Joue(plateau,a,'x'),symbolJoueur))
+#     return value
 
+# def MinValue(plateau,symbolJoueur):
+#     """retourne le min des options parmis tous les max restant"""
+#     value=200
+#     if (Terminal_Test(plateau)!=False):
+#         return Utility(plateau,symbolJoueur)
+#     else:
+#         for a in Action(plateau):
+#             value=min(value,MaxValue(Joue(plateau,a,'x'),symbolJoueur))
+#     return value
+
+
+# # pb avec cette fonction car au lieu d'actualiser seulement le statetemp elle actualise aussi le state
+# def Decision(plateau,symbolJoueur):
+#     """retourne la décision de l'action que l'on va jouer sous forme de coordonnées de l'emplacement à jouer"""
+#     #plateau doit rester le même, on ne modifiera que copiePlateau qui servira pour les simulations
+#     copiePlateau=plateau
+#     print("plateau:\n",plateau,sep="")
+#     print("copiePlateau:\n",copiePlateau,sep="")
+    
+#     listePlaces=Action(plateau)
+#     maxAct=listePlaces[0]
+#     valeurMax=MaxValue(copiePlateau,symbolJoueur)
+    
+#     for place in Action(plateau):
+#         print("action: ",place)
+#         if MaxValue(Joue(copiePlateau,place,'x'),symbolJoueur)<valeurMax:
+#             maxAct=place
+#     return maxAct
+
+
+def MinMax(plateau,symbolJoueur):
+    """retourne le meilleur coup"""
+    #--> le meilleur coup est celui qui retourne Utility(joueur)=1 ou qui empeche Utility(adversaire)=1
+    #on privilégie l'attaque si elle permet de gagner
+    #si on ne peut pas gagner, on privilégie la défence si elle permet de ne pas perdre
+    #si on ne peut pas perdre ni gagner, on attaque
+    
+    listePlaces=Action(plateau) #liste des places libres à jouer
+    copiePlateau=copy(plateau)
+    meilleurCoup=listePlaces[0]
+    #on recherche si on a un coup qui peut nous faire gagner
+    for place in listePlaces:
+        if()
+    
+    
+    return meilleurCoup
 
 # %% TESTS
 
-# Tests sur la classe Plateau       
+    
 plateau=Plateau()
-# print(plateau)
-plateauTest=np.array([['x','x','x','o',None,None,None,None,None,None,None,None],
+plateau.tab=np.array([['x','x','x','o',None,None,None,None,None,None,None,None],
                       [None,None,None,None,None,'x',None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
+                      [None,None,None,None,None,'o',None,None,None,None,None,None],
                       [None,None,None,'o',None,'x',None,None,None,None,None,None],
                       [None,None,None,None,None,'x',None,None,'x',None,None,None],
                       [None,None,None,None,None,'o',None,None,'o',None,None,None],
                       [None,None,None,None,None,None,'o',None,None,None,None,None],
                       [None,None,None,'x',None,None,'o',None,None,None,None,None],
                       ['o',None,None,None,'x',None,None,None,None,None,None,None],
-                      [None,'o',None,'x',None,None,None,None,'o',None,None,None],
+                      [None,'o',None,None,None,None,None,None,'o',None,None,None],
                       [None,None,'x',None,None,None,None,None,None,None,None,None],
                       [None,'x',None,'o',None,None,None,None,None,None,None,None]])
 
-# plateauTest=np.array([['x','o','x','x','o','x','o','x','o','x','o','x'],
-#                       ['o','o','x','x','x','o','o','o','x','o','o','o'],
-#                       ['x','x','o','o','o','x','o','o','o','x','x','o'],
-#                       ['x','o','x','x','o','x','x','x','o','o','x','x'],
-#                       ['x','x','o','x','x','x','o','o','x','o','o','o'],
-#                       ['o','o','o','x','x','o','o','x','o','x','x','x'],
-#                       ['o','x','x','o','o','x','x','o','x','o','o','x'],
-#                       ['x','x','o','x','x','o','o','x','o','x','x','o'],
-#                       ['o','o','x','x','x','o','o','o','x','x','o','o'],
-#                       ['x','o','x','x','o','x','x','o','o','o','x','x'],
-#                       ['o','x','o','o','x','o','x','o','x','x','o','o'],
-#                       ['x','o','x','x','o','x','x','x','o','o','x','x']])
-pTest=Plateau(plateauTest)
 
-# Tests sur la fonctions Actions
-print(Action(pTest.tab))
+#Affichache du tableau : MARCHE
+# print(plateau) 
+#Terminal_Test : MARCHE
+# print(Terminal_Test(plateau)) 
 
-# Tests sur la fonctions Result
-print("pTest avant\n",pTest,sep="")
-Result(pTest.tab,[0,5],"x")
-print("pTest après\n",pTest,sep="")
-
-# Tests sur la fonction Terminal Test
-print(Terminal_Test(pTest.tab))
-# print(Terminal_Test(pTest.tab,9,1))
-# print(Terminal_Test(pTest.tab,10,2))
-# print(Terminal_Test(pTest.tab,11,3)) 
-
-print(Utility(pTest.tab,'x'))
-
-#%%
-
-def MaxValue(p):
-    """retourne le max des options que l'adversaire nous laisse jouer (parmis tous les min restant)"""
-    value=-200
-    if (Terminal_Test(p)!=False):
-        print("fin")
-        return Utility(p)
-    else:
-        for a in Action(p):
-            value=max(value,MinValue(Result(p,a,'x')))
-    return value
-
-def MinValue(p):
-    """retourne le min des options parmis tous les max restant"""
-    value=200
-    if (Terminal_Test(p)!=False):
-        return Utility(p)
-    else:
-        for a in Action(p):
-            value=min(value,MaxValue(Result(p,a,'x')))
-    return value
-
-# pb avec cette fonction car au lieu d'actualiser seulement le statetemp elle actualise aussi le state
-def Decision(state2):
-    """prend la décision de l'action que l'on va jouer"""
-    #les print sont juste des tests
-    #POURQUOI STATE2 CHANGE ?? STATETEMPP EST UNE SIMULATION
-    statetemp=np.array([x for x in state2])
-    print("statetemppp: ",statetemp)
-    print("stat: ",state2)
-    maxAct=None
-    print("statetemppp: ",statetemp)
-    print("stat: ",state2)
-    valeurMax=MaxValue(statetemp)
-    print("statetemppp: ",statetemp)
-    print("stat: ",state2)
-    
-    print("maxxx",valeurMax)
-    print("stateee: ",state2)
-    for a in Action(state2):
-        print("actionnn: ",a)
-        print("dddddd",MaxValue(Result(statetemp,a,'x')))
-        if MaxValue(Result(statetemp,a,'x'))==valeurMax:
-            maxAct=a
-    return maxAct
+#Méthode Action : MARCHE
+#print(Action(plateau.tab))
  
-# %% TESTS
+#Méthode Result : MARCHE
+# print("pTest avant\n",plateau,sep="")
+# Result(plateau.tab,[0,5],"x")
+# print("pTest après\n",plateau,sep="")
 
-plateauTest=np.array([[None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,None],
-                      [None,None,None,None,None,None,None,None,None,None,None,'x'],
-                      [None,None,None,None,None,None,None,None,None,None,None,'x'],
-                      [None,None,None,None,None,None,None,None,None,None,None,'x']])
-pTest=Plateau(plateauTest)
+#Méthode utility : MARCHE
+#print(Utility(plateau,'x')) 
 
-print(Decision(pTest.tab))  
+
+print(Decision(plateau,'x'))  
 # print(Utility(pTest.tab))  
